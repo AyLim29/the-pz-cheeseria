ef6806ce78ac326b54e1ab33ab3da8ecd5cf53f3
AyLim29 <110358602+AyLim29@users.noreply.github.com>
2024-10-15 16:46:58 +1000

    Add initial Spring Boot starter files and dependencies

---
+++ cheeseria-backend/src/main/java/com/cheeseria/backend/CheeseriaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cheeseria.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheeseriaBackendApplication.class, args);
+	}
+
+}
---
+++ cheeseria-backend/src/test/java/com/cheeseria/backend/CheeseriaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheeseria.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeseriaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
