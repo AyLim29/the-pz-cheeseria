e6b4713b2c8d6b57635fbdf61ab7089e14fea83a
AyLim29 <110358602+AyLim29@users.noreply.github.com>
2024-10-16 18:36:57 +1000

    feat: Added weight / price calculator

--- cheeseria-frontend/src/App.js
+++ cheeseria-frontend/src/App.js
@@ -1,9 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import { Button, Container, Form, FormGroup, Input, Label, Offcanvas, OffcanvasHeader, OffcanvasBody} from 'reactstrap';
 
 import './App.css';
 
 const App = () => {
   const [cheeses, setCheeses] = useState([]);
+  const [selectedCheese, setSelectedCheese] = useState(null);
+  const [showCalculator, setShowCalculator] = useState(false);
+  const [calculatedPrice, setCalculatedPrice] = useState(0);
+  const toggleCalculator = () => setShowCalculator(!showCalculator);
+  const handleCheeseButton = (cheese) => {
+    setShowCalculator(true);
+    setSelectedCheese(cheese);
+  };
+  const handlePriceCalculation = (value) => {
+    if (selectedCheese && value) {
+      setCalculatedPrice(selectedCheese.pricePerKilo * (+value));
+    } else {
+      setCalculatedPrice(0);
+    }
+  };
 
   useEffect(() => {
     fetch('/api/getAllCheeses')
@@ -14,6 +30,38 @@ const App = () => {
 
   return (
     <div className="App">
+      <Offcanvas
+        isOpen={showCalculator}
+        direction="end"
+        fade={false}
+        toggle={toggleCalculator}
+      >
+        <OffcanvasHeader
+          toggle={toggleCalculator}>
+          <h2>Price Calculator</h2>
+        </OffcanvasHeader>
+        <OffcanvasBody>
+          { selectedCheese ? (
+            <div className='OffcanvasBodyText'>
+            <strong>
+              {selectedCheese.name}
+            </strong>
+            <p>${selectedCheese.pricePerKilo} / kg</p>
+              <Form>
+                <Input
+                  type="number"
+                  onChange={e => handlePriceCalculation(Number(e.target.value))}
+                  placeholder='Enter kgs'
+                /> 
+              </Form>
+              <h2>Price: ${parseFloat(calculatedPrice).toFixed(2)}</h2>
+            </div>
+          ) : (
+            <p> select a cheese to see its details.</p>
+          )}
+        </OffcanvasBody>
+      </Offcanvas>
+
       <h1>The PZ Cheeseria</h1>
       <table className='center'>
         <thead>
@@ -22,6 +70,7 @@ const App = () => {
             <th>Name</th>
             <th>Price Per Kilo</th>
             <th>Color</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -33,6 +82,20 @@ const App = () => {
               <td>{cheese.name}</td>
               <td>${cheese.pricePerKilo} / kg</td>
               <td>{cheese.colour}</td>
+              <td>
+                <div>
+                  <Button 
+                    color="primary"
+                    onClick={() => {
+                        handlePriceCalculation();
+                        handleCheeseButton(cheese);
+                      }
+                    }
+                  >
+                    Calculate Price
+                  </Button>
+                </div>
+                </td>
             </tr>
           ))}
         </tbody>
